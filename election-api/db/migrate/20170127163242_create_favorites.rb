5cd75764c929a5d51ed47c8d74c8342cfe544d0b
Mario Diaz <mariodiar@gmail.com>
2017-01-27 18:23:29 -0600

    Add favorites relationship to user - proyection

--- election-api/db/migrate/20170127163242_create_favorites.rb
+++ election-api/db/migrate/20170127163242_create_favorites.rb
@@ -2,6 +2,10 @@ class CreateFavorites < ActiveRecord::Migration[5.0]
   def change
     create_table :favorites do |t|
 
+      #Foreign Keys
+      t.integer :user_id
+      t.integer :proyection_id
+      
       t.timestamps
     end
   end
