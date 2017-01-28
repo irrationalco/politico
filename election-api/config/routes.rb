a0d875b234395af5d2b8afc25cf75bb7235207f2
Mario Diaz <mariodiar@gmail.com>
2017-01-27 18:25:24 -0600

    Add maps models, files, etc

--- election-api/config/routes.rb
+++ election-api/config/routes.rb
@@ -1,6 +1,7 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
+  resources :maps
   namespace :api, defaults: { format: 'json' } do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: :true) do
       resources :users
