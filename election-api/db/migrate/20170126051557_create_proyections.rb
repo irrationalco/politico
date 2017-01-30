77d045a4b8b26324a8f8812b632288c7390f43de
Mario Diaz <mariodiar@gmail.com>
2017-01-29 23:39:24 -0600

    Add attributes to maps model

--- election-api/db/migrate/20170126051557_create_proyections.rb
+++ election-api/db/migrate/20170126051557_create_proyections.rb
@@ -1,12 +1,11 @@
 class CreateProyections < ActiveRecord::Migration[5.0]
   def change
     create_table :proyections do |t|
-      t.string :type
-      t.integer :map_key
       t.timestamps
 
       # Foreign Keys
       t.integer :organization_id
+      t.integer :map_id
     end
   end
 end
--- election-api/db/migrate/20170128002503_create_maps.rb
+++ election-api/db/migrate/20170128002503_create_maps.rb
@@ -1,6 +1,9 @@
 class CreateMaps < ActiveRecord::Migration[5.0]
   def change
     create_table :maps do |t|
+      t.string :type
+      t.string :name
+      t.integer :map_key
 
       t.timestamps
     end
