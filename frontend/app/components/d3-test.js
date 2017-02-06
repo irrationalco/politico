f3c5a1c2305809fecfe95b78e27899d41233860a
Mario Diaz <mariodiar@gmail.com>
2017-02-05 23:58:10 -0600

    Remove unused functions from d3-test component

--- frontend/app/components/d3-test.js
+++ frontend/app/components/d3-test.js
@@ -74,35 +74,5 @@ export default Ember.Component.extend({
 				.attr("class", "mesh")
 				.attr("d", path);
 		});
-
-		function clicked(d) {
-			if (active.node() === this) return reset();
-			active.classed("active", false);
-			active = d3.select(this).classed("active", true);
-
-			var bounds = path.bounds(d),
-			dx = bounds[1][0] - bounds[0][0],
-			dy = bounds[1][1] - bounds[0][1],
-			x = (bounds[0][0] + bounds[1][0]) / 2,
-			y = (bounds[0][1] + bounds[1][1]) / 2,
-			scale = .9 / Math.max(dx / width, dy / height),
-			translate = [width / 2 - scale * x, height / 2 - scale * y];
-
-			g.transition()
-			.duration(750)
-			.style("stroke-width", 1.5 / scale + "px")
-			.attr("transform", "translate(" + translate + ")scale(" + scale + ")");
-		}
-
-		function reset() {
-			active.classed("active", false);
-			active = d3.select(null);
-
-			g.transition()
-			.duration(750)
-			.style("stroke-width", "1.5px")
-			.attr("transform", "");
-		}
-
 	}
 });
