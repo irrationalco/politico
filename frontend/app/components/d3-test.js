d130666cc8cdb9f5e66daa9c628daafaf662e2b7
Mario Diaz <mariodiar@gmail.com>
2017-02-11 01:15:48 -0600

    Add tile to mexico inital map

--- frontend/app/components/d3-test.js
+++ frontend/app/components/d3-test.js
@@ -9,8 +9,6 @@ export default Ember.Component.extend({
 
 	didInsertElement() {
 
-		let some = d3Tile.tile();
-
 		// Setting width and height of map container
 		let width = Ember.$("#map").width();
 		let height = Ember.$("#map").height();
@@ -18,16 +16,57 @@ export default Ember.Component.extend({
 
 		// Settings of the map projection
 		// Mexico states projection
+		let pi = Math.PI;
+		let tau = 2 * pi;
+
+		// let projection = d3.geoMercator()
+		// 	.center([-100, 23])
+		// 	.scale(1700)
+		// 	.translate([width / 2, height / 2]);
+
 		let projection = d3.geoMercator()
-			.center([-100, 23])
-			.scale(1700)
-			.translate([width / 2, height / 2]);
+			.scale(1 / tau)
+			.translate([0,0]);
+
+		// Bounding Box Mexico
+		let mexicoBounds = [[-116.2, 12.87],[-88.15, 32.82]],
+		 	p0 = projection([mexicoBounds[0][0], mexicoBounds[1][1]]),
+    		p1 = projection([mexicoBounds[1][0], mexicoBounds[0][1]]);
+
+    	// Convert this to a scale k and translate tx, ty for the projection.
+		// For crisp image tiles, clamp to the nearest power of two.
+		let k = Math.floor(0.95 / Math.max((p1[0] - p0[0]) / width, (p1[1] - p0[1]) / height)),
+		tx = (width - k * (p1[0] + p0[0])) / 2,
+		ty = (height - k * (p1[1] + p0[1])) / 2;
+
+		projection
+		.scale(k / tau)
+		.translate([tx, ty]);
+
+		// Lastly convert this to the corresponding tile.scale and tile.translate;
+		// see http://bl.ocks.org/mbostock/4150951 for a related example.
+		var tiles = d3Tile.tile()
+		.size([width, height])
+		.scale(k)
+		.translate([tx, ty])
+		();
+
+
+		d3.select("#tiles")
+		.selectAll("img").data(tiles).enter().append("img")
+		.style("position", "absolute")
+		.attr("src", function(d, i) { return "http://" + "abc"[d[1] % 3] + ".tile.openstreetmap.org/" + d[2] + "/" + d[0] + "/" + d[1] + ".png"; })
+		.style("left", function(d) { return (d[0] + tiles.translate[0]) * tiles.scale + "px"; })
+		.style("top", function(d) { return (d[1] + tiles.translate[1]) * tiles.scale + "px"; })
+		.attr("width", tiles.scale)
+		.attr("height", tiles.scale);
 
 		let path = d3.geoPath().projection(projection);
 		// let path = d3.geoPath();
 
 		// Initializing SVG on the html element
 		let svg = d3.select("#map").append("svg")
+			.attr("class", "svg-map")
 			.attr("width", width)
 			.attr("height", height);
 
@@ -44,12 +83,9 @@ export default Ember.Component.extend({
 		let gMunicipalities = svg.append("g")
 			.style("stroke-width", "1px");
 
-		// "https://d3js.org/us-10m.v1.json"
 		// Getting topojson data
 		d3.json("../assets/mx_tj.json", (error, data) => {
 			if (error) { console.log(error); }
-
-			this.set('mapData', data);
 
 			gStates.selectAll("path")
 				.data(topojson.feature(data, data.objects.states).features)
@@ -66,9 +102,6 @@ export default Ember.Component.extend({
 
 		// Zooming to bounding box when clicked
 		function clicked(d) {
-
-			console.log(d);
-
 			if (active.node() === this) {
 				return reset();
 			} else {
