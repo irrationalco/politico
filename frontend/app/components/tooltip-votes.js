96f771dd06f9a3f8b6e1ded5770494e2fda52483
Mario Diaz <mariodiar@gmail.com>
2017-04-26 01:45:52 -0500

    Display backend projections data in map

--- frontend/app/components/tooltip-votes.js
+++ frontend/app/components/tooltip-votes.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	
+	sectionData: Ember.computed('hoveredSection', function() {
+		if (this.get('hoveredSection') !== null) {
+			let section = this.get('sectionsData').filterBy('sectionCode', this.get('hoveredSection').section_code);
+			return section[0];	
+		}
+	})
 });
