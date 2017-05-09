90b0051cc6fecbb59e10d99dda2fb81406b9c194
Mario Diaz <mariodiar@gmail.com>
2017-05-08 23:47:15 -0500

    Add ember-concurrency

--- frontend/app/components/tooltip-votes.js
+++ frontend/app/components/tooltip-votes.js
@@ -1,12 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+	color: "Red",
+
+	parties: ["PRI", "PAN", "PRD", "Morena"],
+
 	sectionData: Ember.computed('hoveredSection', function() {
 		if (this.get('hoveredSection') !== null) {
 			let section = this.get('sectionsData').findBy('sectionCode', this.get('hoveredSection').section_code);
-			return section;	
+			return section;
+			this.findTopParties();
 		} else {
 			return null;
 		}
-	})
+	}),
+
+	findTopParties() {
+		this.get("parties").forEach(party => {
+			console.log(this.get('sectionData')("PRI"));
+		});
+	}
 });
