f9fbbc1324919ddb83d9ca417b31c31739c53200
Mario Diaz <mariodiar@gmail.com>
2017-05-22 23:57:29 -0500

    Use parties service data to feed parties-selection component

--- frontend/app/components/parties-selection.js
+++ frontend/app/components/parties-selection.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	partiesManager: Ember.inject.service("parties"),
 
-	parties: Ember.computed.alias('partiesManager.partiesNames'),
+	parties: Ember.computed.alias('partiesManager.parties'),
 	selectedParties: Ember.computed.alias('partiesManager.selectedParties'),
 
 	selectedPartiesChanged: Ember.observer("selectedParties.[]", function() {
