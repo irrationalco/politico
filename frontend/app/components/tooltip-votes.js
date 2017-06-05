bb2dc855089a4c9ef90cf988c779b2670d0b2c95
Mario Diaz <mariodiar@gmail.com>
2017-06-05 14:41:49 -0500

    Add single party tooltip and improve population tooltip

--- frontend/app/components/tooltip-votes.js
+++ frontend/app/components/tooltip-votes.js
@@ -10,6 +10,11 @@ export default Ember.Component.extend({
 	thirdParty:  null,
 	others: 	 null,
 
+	more30: null,
+	less30: null,
+	masc: null,
+	fem: null,
+
 	isSingle: Ember.computed('visualization', function() {
 		if (this.get('visualization') === "single") {
 			return true;
@@ -24,6 +29,18 @@ export default Ember.Component.extend({
 		} else {
 			return false;
 		}
+	}),
+
+	calculateShit: Ember.observer('hoveredSection', function() {
+		let less30 = Math.floor(Math.random() * 100) + 1;
+		let more30 = 100 - less30;
+		let fem = Math.floor(Math.random() * 100) + 1;
+		let masc = 100 - fem;
+
+		this.set('less30', less30);
+		this.set('more30', more30);
+		this.set('fem', fem);
+		this.set('masc', masc);
 	}),
 
 	comparisonBar: Ember.computed('others', function() {
