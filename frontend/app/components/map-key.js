7e056d44721db24cf1990581810f9ed3e108ceb5
Mario Diaz <mariodiar@gmail.com>
2017-06-04 22:47:01 -0500

    Add gradient even key to mapkey when comparing 2 parties

--- frontend/app/components/map-key.js
+++ frontend/app/components/map-key.js
@@ -10,7 +10,16 @@ export default Ember.Component.extend({
 		let colors = this.get('partiesManager').get('colors');
 		let arrParties = [];
 
-		if (selParties.length >= 4) {
+		if(selParties.length === 2) {
+			let party1 = { name: selParties[0], color: colors[selParties[0]] };
+			let party2 = { name: selParties[1], color: colors[selParties[1]] };
+			let gColor = this.get('partiesManager').getGradientColor(party1.color, party2.color);
+			let midKey = { name: "Empate", color: "#" + gColor };
+			arrParties.push(party1);
+			arrParties.push(midKey);
+			arrParties.push(party2);
+			
+		} else if (selParties.length >= 4) {
 			for (var i = 0; i < 4; i++) {
 				let party = { name: selParties[i], color: colors[selParties[i]] };
 				arrParties.push(party);
@@ -21,6 +30,7 @@ export default Ember.Component.extend({
 				arrParties.push(party);
 			}
 		}
+
 		return arrParties;
 	})
 });
