6938b8b8ed32a3995b35d3664926e32378fc7c2a
Mario Diaz <mariodiar@gmail.com>
2017-06-14 22:08:44 -0500

    Add top label to slider

--- frontend/app/components/poll-first.js
+++ frontend/app/components/poll-first.js
@@ -4,18 +4,29 @@ import nouislider from "npm:nouislider";
 export default Ember.Component.extend({
 
 	didInsertElement() {
-		let slider = document.getElementById('myslider');
+		let slider1 = document.getElementById('slider1');
+		let slider2 = document.getElementById('slider2');
+		let slider3 = document.getElementById('slider3');
 
-		nouislider.create(slider, {
+		nouislider.create(slider1, {
 			start: 0,
 			range: {
 				'min': -100,
 				'max': 100
-			},
-			pips: {
-				mode: 'values',
-				values: [],
-				density: 4
+			}
+		});
+		nouislider.create(slider2, {
+			start: 0,
+			range: {
+				'min': -100,
+				'max': 100
+			}
+		});
+		nouislider.create(slider3, {
+			start: 0,
+			range: {
+				'min': -100,
+				'max': 100
 			}
 		});
 	}
