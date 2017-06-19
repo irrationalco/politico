938421a1a6ee3613f35a45723f190d36ea603600
Mario Diaz <mariodiar@gmail.com>
2017-06-19 00:45:12 -0500

    Make slider-value follow handle position & encapsulate the slider in a component

--- frontend/app/components/poll-first.js
+++ frontend/app/components/poll-first.js
@@ -2,22 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-	rangeElement: null,
-
-	// didInsertElement() {
-	// 	this._super(...arguments);
-	// 	let e = this.$().get(0).getElementsByClassName("noUi-origin");
-	// 	this.set('element', )
-	// },
-
-	actions: {
-		setSecond: function(val) {
-			let e = this.$().get(0);
-			console.log(e);
-			let test = this.$().get(0).getElementsByClassName("noUi-origin");
-			console.log(test);
-			console.log(val);
-		}
-	}
 });
 
--- frontend/app/components/poll-slider.js
+++ frontend/app/components/poll-slider.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	sliderValue: null,
+	handle: null,
+
+	didInsertElement() {
+		this.set('handle', this.$().get(0).getElementsByClassName("noUi-origin")[0]);
+		this.set('sliderValue', this.$().get(0).getElementsByClassName("slider-value")[0]);
+	},
+
+	actions: {
+		setSecond: function(val) {
+			let leftValue = this.get('handle').style.left.substring(0,4);
+			leftValue = parseFloat(leftValue) - 2.5;
+			this.get('sliderValue').style.left = leftValue + "%";
+		}
+	}
 });
