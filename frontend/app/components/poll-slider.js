a63412a49867e05dace3f28d33d33a93e5864bcd
Mario Diaz <mariodiar@gmail.com>
2017-06-21 22:31:53 -0500

    Update slider value when moving it

--- frontend/app/components/poll-slider.js
+++ frontend/app/components/poll-slider.js
@@ -1,18 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	sliderValue: null,
+	sliderElement: null,
 	handle: null,
+	value: 0,
 
 	didInsertElement() {
 		this.set('handle', this.$().get(0).getElementsByClassName("noUi-origin")[0]);
-		this.set('sliderValue', this.$().get(0).getElementsByClassName("slider-value")[0]);
+		this.set('sliderElement', this.$().get(0).getElementsByClassName("slider-value")[0]);
 	},
 
 	actions: {
-		setSecond: function(val) {
-			let leftValue = parseFloat(this.get('handle').style.left.substring(0,4)) - 2.5;
-			this.get('sliderValue').style.left = leftValue + "%";
+		setValue: function(val) {
+			let styleLeftValue = parseFloat(this.get('handle').style.left.substring(0,4)) - 2.5;
+			this.get('sliderElement').style.left = styleLeftValue + "%";
+			this.set('value', Math.round(val));
 		}
 	}
 });
