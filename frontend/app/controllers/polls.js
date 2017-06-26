3f84e2976d8b2d15f2851adce3f04e0a53037af7
Mario Diaz <mariodiar@gmail.com>
2017-06-25 20:54:44 -0500

    Integrating poll with different sections & add poll toolbar to navigate through sections

--- frontend/app/controllers/polls.js
+++ frontend/app/controllers/polls.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	totalSections: 7,
+
+	currentSection: 1,
+
+	actions: {
+		setCurrentSection: function(section) {
+			this.set('currentSection', section);
+		}
+	}
 });
