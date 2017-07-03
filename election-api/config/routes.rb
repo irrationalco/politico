09fb012c3dde5aba77194db3500f1c169f437037
Mario Diaz <mariodiar@gmail.com>
2017-07-02 23:51:29 -0500

    Add welcome page

--- election-api/config/routes.rb
+++ election-api/config/routes.rb
@@ -1,6 +1,9 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
+
+  root 'welcome#index'
+  
   namespace :api, defaults: { format: 'json' } do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: :true) do
       resources :users
