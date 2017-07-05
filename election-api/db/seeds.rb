ec7dbbfe2a89c67cfdda490c374ec0cf139e9cf6
Mario Diaz <mariodiar@gmail.com>
2017-07-04 19:26:32 -0500

    Seed historical electoral data

--- election-api/db/seeds.rb
+++ election-api/db/seeds.rb
@@ -28,7 +28,37 @@ require 'csv'
 # end
 
 CSV.foreach("tbl_ine.csv") do |row|
-  puts row.inspect
+  state_code    = row[2]
+  muni_code     = row[4] 
+  section_code  = row[8]
+  district_code = row[7]
+  nominal_list  = row[9]
+  year          = row[0]
+  election_type = row[1]
+
+  pan    = row[10]
+  pconv  = row[11]
+  pdsppn = row[12]
+  pes  = row[13]
+  ph   = row[14]
+  pmc  = row[15]
+  pmor = row[16]
+  pna  = row[17]
+  ppm  = row[18]
+  prd  = row[19]
+  pri  = row[20]
+  psd  = row[21]
+  psm  = row[22]
+  psn  = row[23]
+  pt   = row[24]
+  pvem = row[25]
+
+
+
+  Projection.create(state_code: state_code, muni_code: muni_code, section_code: section_code, district_code: district_code,
+                    nominal_list: nominal_list, year: year, election_type: election_type, 
+                    PAN: pan, PCONV: pconv, PDSPPN: pdsppn, PES: pes, PH: ph, PMC: pmc, PMOR: pmor, PNA: pna, PPM: ppm, PRD: prd,
+                    PRI: pri, PSD: psd, PSM: psm, PSN: psn, PT: pt, PVEM: pvem)
 end
 
 # entidad, distrito, seccion, pobtat, muni
@@ -40,3 +70,9 @@ end
 
 # 10    11      12    13   14  15    16   17   18   19   20   21   22   23   24   25
 # PAN, PCONV, PDSPPN, PES, PH, PMC, PMOR, PNA, PPM, PRD, PRI, PSD, PSM, PSN, PT, PVEM 
+
+
+
+
+
+
