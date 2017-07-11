45134ba551e9fc4ca8f2cc2880597a4a73de1832
Andres de Lago Gomez <delagoandres@gmail.com>
2017-07-10 22:28:05 -0500

    Adds initial filter capabilities
    Line graphs don't show 'other' yet

--- frontend/app/components/projections-charts.js
+++ frontend/app/components/projections-charts.js
@@ -1,69 +1,146 @@
 import Ember from 'ember';
-import { task } from 'ember-concurrency';
+import {
+  task
+} from 'ember-concurrency';
 
 export default Ember.Component.extend({
 
   actions: {
-      toggle: function(){
-          $("#charts-panel").animate({right: (this.isOpen?"-250":"0")},()=>this.set('isStatic', true));
-          this.set('isStatic', false);
-          this.set('isOpen', !this.isOpen);
-          this.get('loadChartData').perform();
-      }
+    toggle: function () {
+      $("#charts-panel").animate({
+        right: (this.isOpen ? "-250" : "0")
+      }, () => this.set('isStatic', true));
+      this.set('isStatic', false);
+      this.set('isOpen', !this.isOpen);
+      this.get('loadChartData').perform();
+    }
   },
 
   isOpen: false,
 
   isStatic: true,
 
-  parties: ["PAN","PCONV","PES","PH","PMC","PMOR","PNA",
-            "PPM","PRD","PRI","PSD","PSM","PT","PVEM"],
+  parties: ["PAN", "PCONV", "PES", "PH", "PMC", "PMOR", "PNA",
+    "PPM", "PRD", "PRI", "PSD", "PSM", "PT", "PVEM"
+  ],
 
   colors: {
-      PRI: "#00923f",
-      PAN: "#06338e",
-      PCONV: "#e38129",
-      PMOR: "#93000a",
-      PRD: "#ffcb01",
-      PVEM: "#95c065",
-      PT: "#da251d",
-      PMC: "#f04c23",
-      PES: "#490073",
-      PH: "#9f3b77",
-      PNA: "#00a4ac",
-      PSD: "#ff0b06",
-      PPM: "#00adef",
-      PSM: "#f50800"},
+    PRI: "#00923f",
+    PAN: "#06338e",
+    PCONV: "#e38129",
+    PMOR: "#93000a",
+    PRD: "#ffcb01",
+    PVEM: "#95c065",
+    PT: "#da251d",
+    PMC: "#f04c23",
+    PES: "#490073",
+    PH: "#9f3b77",
+    PNA: "#00a4ac",
+    PSD: "#ff0b06",
+    PPM: "#00adef",
+    PSM: "#f50800"
+  },
 
-  formatDataset(party, raw){
-    return {label: party,
+  electionTypes: {
+    dif: 0,
+    prs: 1,
+    sen: 2
+  },
+
+  chartNames: ['deputiesChart', 'presidentChart', 'senatorsChart'],
+
+  formatDataset(party, raw) {
+    return {
+      label: party,
       backgroundColor: this.colors[party],
       fill: false,
       borderColor: this.colors[party],
-      data: raw.map((x)=>x.get(party)),
-      pointBackgroundColor: this.colors[party]};
+      data: raw.map((x) => x.get(party)),
+      pointBackgroundColor: this.colors[party]
+    };
   },
 
-  formatChartData: task(function * (raw){
+  getOther(activeParties) {
+    return this.parties.filter((x) => activeParties.indexOf(x) === -1)
+      .reduce((s, v) => {
+        return s + raw[0].get(v)
+      }, 0);
+  },
+
+  filterPartiesDoughnut(raw) {
+    let total = this.parties.reduce((s, v) => {
+      return s + raw[0].get(v)
+    }, 0);
+    let minVal = total * 0.05;
+    let activeParties = this.parties.filter((x) => raw[0].get(x) >= minVal);
+    activeParties.sort((a, b) => raw[0].get(b) - raw[0].get(a));
+    minVal = total * 0.95;
+    activeParties = activeParties.filter((x) => {
+      let tmp = minVal;
+      minVal -= raw[0].get(x);
+      return tmp >= 0;
+    });
+    return {
+      active: activeParties,
+      other: this.parties.filter((x) => activeParties.indexOf(x) === -1)
+        .reduce((s, v) => {
+          return s + raw[0].get(v)
+        }, 0)
+    };
+  },
+
+  filterPartiesLine(raw) {
+    // let activeParties = this.parties.filter((party)=>
+    //   raw.map((x)=>x.get(party)).reduce((s,v)=>{s+v},0)!==0);
+    //   !raw.map((x)=>x.get(party)).some((v)=>v===0));
+    let ratios = this.parties.map((s) => 0);
+    raw.forEach((year, idx) => {
+      let total = this.parties.reduce((s, v) => {
+        return s + year.get(v)
+      }, 0);
+      this.parties.forEach((party, index) => ratios[index] += year.get(party) / total * (idx + 1));
+    });
+    let total = ratios.reduce((s, v) => {
+      return s + v
+    }, 0);
+    ratios = ratios.map((x, idx) => {
+      return {
+        party: this.parties[idx],
+        ratio: x / total
+      };
+    });
+    let activeParties = ratios.filter((x) => x.ratio >= .05);
+    activeParties.sort((a, b) => b.ratio - a.ratio);
+    let val = 0;
+    activeParties = activeParties.filter((x) => {
+      let tmp = val;
+      val += x.ratio;
+      return tmp <= .95;
+    }).map((x) => x.party);
+    return {
+      active: activeParties,
+      other: this.parties.filter((x) => activeParties.indexOf(x) === -1)
+    };
+  },
+
+  formatChartData: task(function* (raw) {
     let result = null;
-    if(raw.length === 1){
-      let activeParties = this.parties.filter((x)=>raw[0].get(x)>0);
+    if (raw.length === 1) {
+      let parties = this.filterPartiesDoughnut(raw);
       result = {
-        labels: activeParties,
+        labels: parties.active.concat('otros'),
         datasets: [{
           label: "Votos",
-          data: activeParties.map((x)=>raw[0].get(x)),
-          backgroundColor: activeParties.map((x)=>this.colors[x])
+          data: parties.active.map((x) => raw[0].get(x)).concat(parties.other),
+          backgroundColor: parties.active.map((x) => this.colors[x]).concat('#4d4d4d')
         }]
       };
-    }else{
-      raw = raw.sort((a,b)=>a.get('year')-b.get('year'));
-      let activeParties = this.parties.filter((party)=>
-        raw.map((x)=>x.get(party)).reduce((s,v)=>{s+v},0)!==0);
-        // !raw.map((x)=>x.get(party)).some((v)=>v===0));
+    } else {
+      raw = raw.sort((a, b) => a.get('year') - b.get('year'));
+      let parties = this.filterPartiesLine(raw);
       result = {
-        labels: raw.map((x)=>x.get('year')),
-        datasets: activeParties.map((x)=>this.formatDataset(x,raw))
+        labels: raw.map((x) => x.get('year')),
+        datasets: parties.active.map((x) => this.formatDataset(x, raw))
       };
     }
     return result;
@@ -81,38 +158,38 @@ export default Ember.Component.extend({
 
   deputiesChartType: null,
 
-  electionTypes: {dif: 0, prs: 1, sen: 2},
-
-  chartNames: ['deputiesChart', 'presidentChart', 'senatorsChart'],
-
-  setChart: task(function * (chartName, data) {
-    this.set(chartName ,yield this.get('formatChartData').perform(data));
-    this.set(chartName+'Type', this.get(chartName).datasets.length===1?"doughnut":"line");
+  setChart: task(function* (chartName, data) {
+    this.set(chartName, yield this.get('formatChartData').perform(data));
+    this.set(chartName + 'Type', this.get(chartName).datasets.length === 1 ? "doughnut" : "line");
   }),
 
-  loadChartData: task(function * () {
-    if(!this.isOpen){
+  loadChartData: task(function* () {
+    if (!this.isOpen) {
       return;
     }
     let data = null;
-    if(this.get("level") === "section"){
+    if (this.get("level") === "section") {
       let result = yield this.get('store').query('projection', {
         history: 1,
         state: this.get('state'),
         section: this.get('section')
       });
-      data = [[],[],[]];
+      data = [
+        [],
+        [],
+        []
+      ];
       result.forEach((item) => {
         data[this.electionTypes[item.get('electionType')]].push(item);
       });
     }
-    if(!data){
-      this.chartNames.forEach((name)=> this.set(name, null));
+    if (!data) {
+      this.chartNames.forEach((name) => this.set(name, null));
       return;
     }
     let charts = this.chartNames.map((item, index) =>
       this.get('setChart').perform(item, data[index]));
-    for(let i=0;i<charts.length;i++){
+    for (let i = 0; i < charts.length; i++) {
       charts[i] = yield charts[i];
     }
   }).restartable(),
