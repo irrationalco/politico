7beb9d59c277825f9c96d4db7c172656f284cb66
Mario Diaz <mariodiar@gmail.com>
2017-07-11 21:22:00 -0500

    Add states and municipalities models and tables

--- election-api/db/schema.rb
+++ election-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170630025240) do
+ActiveRecord::Schema.define(version: 20170712021603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20170630025240) do
     t.integer  "projection_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "municipalities", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "muni_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "state_id"
   end
 
   create_table "organizations", force: :cascade do |t|
@@ -72,6 +80,13 @@ ActiveRecord::Schema.define(version: 20170630025240) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "poll_id"
+  end
+
+  create_table "states", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "state_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
