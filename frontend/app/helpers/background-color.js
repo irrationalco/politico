47e6e7fa5e4ca4d12a30c0577181b07d2627bed1
Mario Diaz <mariodiar@gmail.com>
2017-07-18 00:07:12 -0500

    Display party colors on the tooltip

--- frontend/app/helpers/background-color.js
+++ frontend/app/helpers/background-color.js
@@ -6,4 +6,5 @@ export function backgroundColor([color, ...rest]) {
 	let hexColor = isEmpty(color) ? "#cccccc" : color;
 	return Ember.String.htmlSafe("background-color: " + hexColor);
 }
+
 export default Ember.Helper.helper(backgroundColor);
