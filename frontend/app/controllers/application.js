67a7fe9e0329ba061951f6c5d727b3841d5c4ef2
Mario Diaz <mariodiar@gmail.com>
2017-07-19 23:01:25 -0500

    Add changes to deliver to client hilda

--- frontend/app/controllers/application.js
+++ frontend/app/controllers/application.js
@@ -1,15 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	sidebarOpen: true,
-
-	actions: {
-		toggleSidebar() {
-			if (this.get('sidebarOpen')) {
-				this.set('sidebarOpen', false);
-			} else {
-				this.set('sidebarOpen', true);
-			}
-		}
-	} 
+	
 });
--- frontend/app/controllers/authenticated.js
+++ frontend/app/controllers/authenticated.js
@@ -2,4 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   auth: Ember.inject.service(),
+  sidebarOpen: true,
+
+	actions: {
+		toggleSidebar() {
+			if (this.get('sidebarOpen')) {
+				this.set('sidebarOpen', false);
+			} else {
+				this.set('sidebarOpen', true);
+			}
+		}
+	} 
 });
