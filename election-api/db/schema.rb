0696a920509c3109305c47c49d87efb9f110b210
Mario Diaz <mariodiar@gmail.com>
2017-08-29 22:12:23 -0500

    Add name to users table

--- election-api/db/schema.rb
+++ election-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170821003128) do
+ActiveRecord::Schema.define(version: 20170830030704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -127,6 +127,8 @@ ActiveRecord::Schema.define(version: 20170821003128) do
     t.datetime "created_at",                                     null: false
     t.datetime "updated_at",                                     null: false
     t.string   "authentication_token",   limit: 30
+    t.string   "first_name",             limit: 35
+    t.string   "last_name",              limit: 35
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
