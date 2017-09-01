ee35ab154dc8c550ee56b9c3f7dc18b5bddef684
Mario Diaz <mariodiar@gmail.com>
2017-09-01 00:52:55 -0500

    Add errors to login form and style logout dropdown

--- frontend/app/components/login-form.js
+++ frontend/app/components/login-form.js
@@ -1,13 +1,16 @@
 import Ember from 'ember';
+
 const { service } = Ember.inject;
+
 export default Ember.Component.extend({
 	session: service('session'),
+	notify:	 service('notify'),
 
 	actions: {
 		authenticate() {
       let { identification, password } = this.getProperties('identification', 'password');
       this.get('session').authenticate('authenticator:oauth2', identification, password).catch((reason) => {
-        this.set('errorMessage', reason.error || reason);
+      	this.get('notify').alert("Error: Asegurate que tu contraseña y usuario sean correctos.", {closeAfter: null});
       });
     }
 	}
