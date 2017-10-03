ee40d7d725c59b3500448768cbd596933ca65ed1
Mario Diaz <mariodiar@gmail.com>
2017-10-02 21:42:23 -0500

    Fix issue when getting manager name

--- election-api/app/serializers/suborganization_serializer.rb
+++ election-api/app/serializers/suborganization_serializer.rb
@@ -1,11 +1,10 @@
 class SuborganizationSerializer < ActiveModel::Serializer
-  attributes :id, :name, :manager_id, :manager_name, :organization_id, :organization_name
+  attributes :id, :name, :manager_id, :manager_name, :organization_id
 
   belongs_to :organization
 
   def manager_name
-    manager = User.find(object.manager_id)
-    name = manager.first_name + " " + manager.last_name
-    name
+    manager = object.manager
+    manager.present? ? manager.first_name + " " +manager.last_name : nil
   end
 end
