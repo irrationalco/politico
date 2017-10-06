f998ad10bda0192ff2abd541dee53476274e2621
Mario Diaz <mariodiar@gmail.com>
2017-10-05 22:52:42 -0500

    Fix typo in users serializer

--- frontend/app/models/user.js
+++ frontend/app/models/user.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
 	email: 		 	DS.attr('string'),
 	firstName: 	DS.attr('string'),
 	lastName:	 	DS.attr('string'),
-	superAdmin: DS.attr('boolean'),
+	superadmin: DS.attr('boolean'),
 	supervisor: DS.attr('boolean'),
 	manager:		DS.attr('boolean'),
 	capturist: 	DS.attr('boolean'),
