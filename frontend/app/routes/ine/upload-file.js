c7ee46a1e7db54c30ad62c816e154e845ae38eb8
Andres de Lago <delagoandres@gmail.com>
2017-10-26 17:02:33 -0500

    Adds a bit of feedback to the upload file controller

--- frontend/app/routes/ine/upload-file.js
+++ frontend/app/routes/ine/upload-file.js
@@ -5,8 +5,32 @@ import { task } from 'ember-concurrency';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
     session: Ember.inject.service(),
+
+    triedToUpload: false,
+
+    uploadSuccesful: false,
+
+    downloadAnswerFile: function(filename, text) {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+    },
+
     fileUploader: task(function* (file) {
         let response = yield file.upload(config.localhost + '/api/voters/file_upload/' + this.get('session.currentUser').id, { accepts: 'text/csv' });
+        console.log(response)
+        this.set('triedToUpload', true);
+        this.set('uploadSuccesful', response['content-type'] !== 'text/csv');
+        if(this.uploadSuccesful){
+            //todos los datos fueron validos
+        }else{
+            //regreso el csv con errores
+            this.downloadAnswerFile('filas invalidas.csv', response.body);
+        }
     }).enqueue(),
 
     actions: {
