f4df5eb1e7c09bdfffa917778e9602f10e36e36e
Mario Diaz <mariodiar@gmail.com>
2017-11-01 23:18:35 -0600

    show expanding rows with transitions

--- frontend/app/transitions.js
+++ frontend/app/transitions.js
@@ -8,4 +8,18 @@ export default function() {
         this.use('toRight', {duration: duration }),
         this.reverse('toLeft', { duration: duration })
     );
+
+    this.transition(
+    	this.hasClass('expandedVoter'),
+
+		  // this makes our rule apply when the liquid-if transitions to the
+		  // true state.
+		  this.toValue(true),
+		  this.use('toDown', {duration: 300}),
+
+		  // which means we can also apply a reverse rule for transitions to
+		  // the false state.
+		  this.reverse('toUp', {duration: 300})
+  	);
 };
+
