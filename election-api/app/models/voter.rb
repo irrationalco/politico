f8d2b500136af7a6ab90e629974aca0479c82f16
Andres de Lago <delagoandres@gmail.com>
2017-11-02 18:19:56 -0600

    Changes file upload-page to use 3 fields for date

--- election-api/app/models/voter.rb
+++ election-api/app/models/voter.rb
@@ -20,41 +20,41 @@ class Voter < ApplicationRecord
           v.first_last_name = row[4].upcase
           v.second_last_name = row[5].upcase
           v.gender = row[6].upcase
-          v.date_of_birth = Date.strptime(row[7] , "%d/%m/%Y")
-          v.electoral_code = row[8].upcase
-          v.CURP = row[9].upcase
+          v.date_of_birth = Date.strptime("#{row[7]}/#{row[8]}/#{row[9]}", "%d/%m/%Y")
+          v.electoral_code = row[10].upcase
+          v.CURP = row[11].upcase
     
-          v.section = row[10].upcase
-          v.street = row[11].upcase
-          v.outside_number = row[12].upcase
-          v.inside_number = row[13].upcase
-          v.suburb = row[14].upcase
-          v.locality_code = row[15].to_i 
-          v.locality = row[16].upcase
-          v.municipality_code = row[17].to_i
-          v.municipality = row[18].upcase
-          v.state_code = row[19].to_i
-          v.state = row[20].upcase
-          v.postal_code = row[21].to_i
+          v.section = row[12].upcase
+          v.street = row[13].upcase
+          v.outside_number = row[14].upcase
+          v.inside_number = row[15].upcase
+          v.suburb = row[16].upcase
+          v.locality_code = row[17].to_i 
+          v.locality = row[18].upcase
+          v.municipality_code = row[19].to_i
+          v.municipality = row[20].upcase
+          v.state_code = row[21].to_i
+          v.state = row[22].upcase
+          v.postal_code = row[23].to_i
           
-          v.home_phone = row[22].to_i
-          v.mobile_phone = row[23].to_i
-          v.email = row[24].upcase
-          v.alternative_email = row[25].upcase
-          v.facebook_account = row[26].upcase
+          v.home_phone = row[24].to_i
+          v.mobile_phone = row[25].to_i
+          v.email = row[26].upcase
+          v.alternative_email = row[27].upcase
+          v.facebook_account = row[28].upcase
     
-          v.highest_educational_level = row[27].upcase
-          v.current_ocupation = row[28].upcase
+          v.highest_educational_level = row[29].upcase
+          v.current_ocupation = row[30].upcase
     
-          v.organization = row[29].upcase
-          v.party_positions_held = row[30].upcase
-          v.is_part_of_party = ActiveModel::Type::Boolean.new.cast row[31].downcase
-          v.has_been_candidate = ActiveModel::Type::Boolean.new.cast row[32].downcase
-          v.popular_election_position = row[33] 
-          v.election_year = row[34].upcase
-          v.won_election = ActiveModel::Type::Boolean.new.cast row[35].downcase
-          v.election_route = row[36].upcase
-          v.notes = row[37].upcase
+          v.organization = row[31].upcase
+          v.party_positions_held = row[32].upcase
+          v.is_part_of_party = ActiveModel::Type::Boolean.new.cast row[33].downcase
+          v.has_been_candidate = ActiveModel::Type::Boolean.new.cast row[34].downcase
+          v.popular_election_position = row[35] 
+          v.election_year = row[36].upcase
+          v.won_election = ActiveModel::Type::Boolean.new.cast row[37].downcase
+          v.election_route = row[38].upcase
+          v.notes = row[39].upcase
         end
       rescue
         p $!.message
@@ -62,16 +62,13 @@ class Voter < ApplicationRecord
       end
     end
     if invalidRows.length > 0
-        attributes = ['Caputrado por',	'Numero de la credencial de elector',	
-        'Vigencia de la credencial de elector', 'Apellido Paterno',	'Apellido Materno',	
-        'Primer Nombre',	'Segundo Nombre',	'Sexo',	'Fecha de nacimiento',	'Clave de elector',	
-        'CURP',	'Seccion Electoral',	'Calle',	'Numero exterior',	'Numero Interior',	'Colonia',	
-        'Clave de la localidad',	'Localidad',	'Clave de municipio',	'Municipio',	'Clave de estado',	
-        'Estado',	'Codigo postal',	'Telefono fijo',	'Telefono celular',	'Correo Electronico',	
-        'Correo electronico alternativo',	'Cuenta Facebook',	'Ultimo Grado de estudios',	
-        'Ocupacion actual',	'Organizacion a la que pertenece',	'Cargos partidarios que ha tenido',	
-        'Pertenece a la estructura del partido',	'Ha sido candidato(a)', 'Cargo de eleccion popular',	
-        'Ano de eleccion',	'Resulto electa(o)',	'Via de eleccion', 'Notas']
+        attributes = ['Numero de la credencial de elector', 'Vigencia de la credencial de elector', 'Apellido Paterno',
+                       'Apellido Materno', 'Primer Nombre', 'Segundo Nombre', 'Sexo', 'Dia de nacimiento', 'Mes de nacimiento',
+                       'Anio de nacimiento','Clave de elector', 'CURP', 'Seccion Electoral', 'Calle', 'Numero exterior', 'Numero Interior',
+                       'Colonia', 'Clave de la localidad', 'Clave de municipio', 'Clave de estado', 'Codigo postal', 'Telefono fijo',
+                       'Telefono celular', 'Correo Electronico', 'Correo electronico alternativo', 'Cuenta Facebook', 'Ultimo Grado de estudios',
+                       'Ocupacion Actual', 'Organizacion a la que pertenece','Cargos partidarios que ha tenido', 'Pertenece a la estructura del partido', 
+                       'Ha sido candidata(o)', 'Cargo de eleccion popular', 'Año de eleccion', 'Resulto electa(o)', 'Via de eleccion', 'Notas']
     
         CSV.generate(headers: true) do |csv|
           csv << attributes
