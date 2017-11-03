f82b4ccc2f2614e802919adc8d01d685aa3b59c0
Mario Diaz <mariodiar@gmail.com>
2017-11-02 22:34:51 -0600

    Mejorando table de voters, lista para estilo

--- frontend/app/controllers/ine/voters2.js
+++ frontend/app/controllers/ine/voters2.js
@@ -4,12 +4,33 @@ import config from '../../config/environment';
 const { service } = Ember.inject;
 
 export default Ember.Controller.extend({
+
+	showFilters: false,
+	showQuickForm: false,
+
 	session: service('session'),
 	ajax: 	 service('ajax'),
 	notify:  service('notify'),
 	store: 	 service('store'),
 
 	actions: {
+
+		showFilters() {
+			if(this.get('showFilters')) {
+				this.set('showFilters', false);
+			} else {
+				this.set('showFilters', true);
+			}
+		},
+
+		showQuickForm() {
+			if(this.get('showQuickForm')) {
+				this.set('showQuickForm', false);
+			} else {
+				this.set('showQuickForm', true);
+			}
+		},
+
 		searchVoters(term) {
 			return this.get('store').query('voter', {name: term});
 		},
