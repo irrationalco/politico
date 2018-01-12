5801af16c90402ec4e9f29f218fa6ef671a0360f
Andres de Lago <delagoandres@gmail.com>
2018-01-12 11:55:04 -0600

    Changes to async chart API

--- frontend/app/adapters/dashboard-info.js
+++
@@ -1,7 +0,0 @@
-import ApplicationAdapter from './application';
-
-export default ApplicationAdapter.extend({
-  pathForType: function (type) {
-    return 'ine/dashboard';
-  }
-});
--- frontend/app/components/async-chart.js
+++ frontend/app/components/async-chart.js
@@ -1,31 +1,94 @@
 import Ember from 'ember';
 import {
-  task
+  task,
+  taskGroup
 } from 'ember-concurrency';
+import config from '../config/environment';
+const {
+  service
+} = Ember.inject;
+const Chartkick = window.Chartkick;
 
 export default Ember.Component.extend({
+
+  ajax: service('ajax'),
+  session: service('session'),
 
   actions: {},
 
   data: null,
 
+  success: false,
+
   error: false,
 
-  runQuery: task(function* () {
-    let result = yield this.get('store').queryRecord('dashboardInfo', {
-      chart: this.get('query')
-    });
-    if (!result) {
+  id: null,
+
+  allTasks: taskGroup().enqueue(),
+
+  runQuery: task(function* (headerName, headerValue) {
+    try {
+      let result = yield this.get('ajax').request(config.localhost + '/api/ine/dashboard', {
+        accepts: {
+          json: 'application/json'
+        },
+        headers: {
+          [headerName]: headerValue
+        },
+        data: {
+          chart: this.get('query')
+        }
+      });
+      if (!result) {
+        this.set('error', true)
+      } else {
+        this.set('success', true);
+        if (Object.keys(result).length !== 0 || result.constructor !== Object) {
+          this.set('data', result);
+          console.log(this.get('data'))
+        } else {
+          console.log(this.get('id'), 'empty')
+        }
+      }
+    } catch (err) {
       this.set('error', true);
-    } else {
-      this.set('data', result.get('info'));
-      console.log(this.get('data'))
     }
-  }),
+  }).group('allTasks'),
+
+  createChart: task(function* () {
+    if (this.get('error') || !this.get('data')) {
+      return;
+    }
+    console.log(this.get('id'), this.get('data'))
+    let type = this.get('type');
+    if (type === "line") {
+      new Chartkick.LineChart(this.get('id'), this.get('data'));
+    } else if (type === "pie") {
+      new Chartkick.PieChart(this.get('id'), this.get('data'), {
+        doughnut: true
+      });
+    } else if (type === "column") {
+      new Chartkick.ColumnChart(this.get('id'), this.get('data'));
+    } else if (type === "bar") {
+      new Chartkick.BarChart(this.get('id'), this.get('data'));
+    } else if (type === "area") {
+      new Chartkick.AreaChart(this.get('id'), this.get('data'));
+    } else if (type === "scatter") {
+      new Chartkick.ScatterChart(this.get('id'), this.get('data'));
+    }
+  }).group('allTasks'),
 
   init() {
     this._super(...arguments);
-    this.get('runQuery').perform();
+    this.set('id', this.get('query') + '-' + Math.floor(Math.random() * 1000));
+    this.get('session').authorize('authorizer:oauth2', (headerName, headerValue) => {
+      this.get('runQuery').perform(headerName, headerValue);
+    });
+  },
+
+  didUpdate() {
+    this._super(...arguments);
+    this.get('createChart').perform();
   }
 
 });
