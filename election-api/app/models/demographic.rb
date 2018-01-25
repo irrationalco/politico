a7094c200edf930a045cdd270164f048462fe908
Mario Diaz <mariodiar@gmail.com>
2018-01-25 00:50:30 -0600

    Disable lambda check in rubocop

--- election-api/app/models/demographic.rb
+++ election-api/app/models/demographic.rb
@@ -1,12 +1,12 @@
 class Demographic < ApplicationRecord
-  scope :municipal, lambda(state_code, muni_code, year) {
+  scope :municipal, ->(state_code, muni_code, year) {
     where([
             'state_code = ? AND muni_code = ?
             AND year = ?', state_code, muni_code, year
           ])
   }
 
-  scope :distrital, lambda(state_code, district_code, year) {
+  scope :distrital, ->(state_code, district_code, year) {
     where([
             'state_code = ? AND district_code = ?
             AND year = ?', state_code, district_code, year
